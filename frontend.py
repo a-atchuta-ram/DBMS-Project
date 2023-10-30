4a6c2ab1fb549ba862d1e19fe47557b3eb284951
Aryan Wadhwa <aryanwadhwa05@gmail.com>
2023-10-30 16:20:28 +0530

    Changing Background

--- frontend.py
+++ frontend.py
@@ -10,33 +10,54 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+st.markdown(
+    """
+    <style>
+    {% include 'design.css' %}
+    </style>
+    , 
+    unsafe_allow_html=True
+    """
+)
+
 
 def main():
 
     st.title("Library Management System")
     options = st.sidebar.selectbox("Select an Operation",("Search Book", "Rent", "Update", "Delete"))
 
-
     if(options == "Search Book"):
         st.subheader("Search a Book")
-        bookName = st.text_input("Enter the name: ")
-        email = st.text_input("Enter Email")
+        bookName = st.text_input("Enter the book name: ")
+        #email = st.text_input("Enter Email")
         if st.button("Search Book"):
-            sql = "insert into users(name, email) values(%s, %s)" ## Change the query
-            val = (bookName, email)
+
+            #sql = "select book_name from users where book_name = %s" ## Change the sql query
+            sql = ""
+            val = (bookName)
             result = mycursor.execute(sql, val)
             if(result):
                 st.subheader("Book Found")
-                st.subheader("Go to the rent page to borrow the book!!")
-                st.write(result)    
+                st.subheader("Go to the rent page to borrow the book!")
+                st.write(result) 
+            else:
+                st.write("Book Not Found!!")   
 
-        if(options == "Rent"):
-            st.subheader("DashBoard")
-            bookName = st.text_input("Enter the book name: ")
-            authorName = st.text_input("Enter the author name: ")
+    if(options == "Rent"):
+        st.subheader("DashBoard")
+        bookName = st.text_input("Enter the book name: ")
+        rentName = st.text_input("Enter your name: ")
+    
+        if st.button("Rent"):
+            sql = ""
+            val = (bookName,rentName)
 
-
-
+            result = mycursor.execute(sql, val)
+            if(result):
+                st.write(result)
+            else:
+                st.write("Something went wrong!!")
+            
 
 
 if __name__ == "__main__":
